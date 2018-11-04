d14ca457d12c0940ffdcb17d42c0d8cbb907d98b
王嘉巍 <906789088@qq.com>
2018-11-04 17:14:40 +0800

    feat:using easily Jsx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+//在React中，当用自己定义的标签时，首字母要大写，若发现有小写的，则为标准的h5语法
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
