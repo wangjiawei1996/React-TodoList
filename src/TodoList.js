befe27bcfbd144be5583e156e3774bf0b65d63a8
王嘉巍 <906789088@qq.com>
2018-11-04 21:44:29 +0800

    feat:inputValue of TodoList

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,35 @@
 import React, { Component, Fragment} from 'react';
 
 class TodoList extends Component {
+  constructor(props) {  //接受props数据
+		super(props);       //调用props这个方法
+		this.state = {
+			inputValue: '',
+			list: []
+		}
+	}
+
     render() {
         return (
             <Fragment>
-							<div><input /><button>提交</button></div>
+							<div>
+								<input
+									value={this.state.inputValue}
+									onChange={this.handleInputChange.bind(this)}
+							/>
+								<button>提交</button>
+							</div>
 							<ul>
 								<li>学习React-TodoList</li>
 								<li>Learning</li>
 							</ul>
 						</Fragment>
         )
-    }
+		}
+		handleInputChange(e){
+			this.setState({
+				inputValue : e.target.value
+			})
+		}
 }
 export default TodoList;
