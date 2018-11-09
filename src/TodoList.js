44a17c4899edc0c220a61f082d1008afd96a5373
王嘉巍 <906789088@qq.com>
2018-11-10 00:14:15 +0800

    feat:store from redux

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,28 +1,27 @@
 import React,{ Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input,Button,List } from 'antd';
+import store from './store/index.js'
 
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
 
 
 class TodoList extends Component{
+	constructor(props){
+		super(props);
+		this.state = store.getState(); 
+		console.log(store.getState());
+	}
     render(){
         return (
           <div style={{marginTop:'10px',marginLeft:'10px'}}>
             <div>
-							<Input placeholder='todo info' style={{width:'300px',marginRight:'10px'}} />
+							<Input value={this.state.inputValue} placeholder='todo info' style={{width:'300px',marginRight:'10px'}} />
 							<Button type="primary">提交</Button>
             </div>
 						<List
 						style={{marginTop:'10px',width:'300px'}}
 							bordered
-							dataSource={data}
+							dataSource={this.state.list}
 							renderItem={item => (<List.Item>{item}</List.Item>)}
 						/>
           </div>)
