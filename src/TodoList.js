46ea5b6858cf8b04342a353f100586bffb404f70
王嘉巍 <906789088@qq.com>
2018-11-11 16:59:38 +0800

    feat:UI组件 and Box组件

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,8 @@
 import React,{ Component } from 'react';
 import 'antd/dist/antd.css';
-import { Input,Button,List } from 'antd';
 import store from './store/index.js';
 import { getInputChangeAction, getAddItemAction, getDeleteItemAction} from './store/actionCreators';
+import TodoListUI from './TodoListUI';
 
 
 
@@ -13,26 +13,19 @@ class TodoList extends Component{
 		this.handleInputChange = this.handleInputChange.bind(this);
 		this.handleStoreChange = this.handleStoreChange.bind(this);
 		this.handleBtnClick = this.handleBtnClick.bind(this);
+		this.handleItemDelete = this.handleItemDelete.bind(this);
 		store.subscribe(this.handleStoreChange)
 	}
     render(){
         return (
-          <div style={{marginTop:'10px',marginLeft:'10px'}}>
-            <div>
-							<Input value={this.state.inputValue}
-							 placeholder='todo info'
-							 style={{width:'300px',marginRight:'10px'}}
-							 onChange={this.handleInputChange}
-							/>
-							<Button type="primary" onClick={this.handleBtnClick}>提交</Button>
-            </div>
-						<List
-						style={{marginTop:'10px',width:'300px'}}
-							bordered
-							dataSource={this.state.list}
-							renderItem={(item, index) => (<List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>)}
-						/>
-          </div>)
+				<TodoListUI
+					inputValue={this.state.inputValue}
+					list = {this.state.list}
+					handleInputChange = {this.handleInputChange}
+					handleBtnClick = {this.handleBtnClick}
+					handleItemDelete = {this.handleItemDelete}
+				/>
+				)
 		}
 		handleInputChange(e){
 			const action = getInputChangeAction(e.target.value)
